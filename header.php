44cdfb262f7a5927f93717c2b9ade69390f6f4ba
alfahrelrifananda <pahrel1234@gmail.com>
2025-11-09 14:25:58 +0800

    update

--- header.php
+++ header.php
@@ -7,7 +7,6 @@ require_once 'config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?? 'AlfahrelRifananda' ?></title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
