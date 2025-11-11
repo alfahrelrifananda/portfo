720cc8647dbf6c02f14688cd89a0bf9854722099
alfahrelrifananda <pahrel1234@gmail.com>
2025-11-11 14:45:04 +0700

    update

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <a href="projects.php">Lihat Proyek</a> |
             <a href="contact.php">Hubungi Saya</a>
         </p>
-        <p>Untuk melihat kode sumber, silahkan klik <a href="https://github.com/alfahrelrifananda/portfo/">disini</a>.</p>
+        <p>Untuk melihat source code dari website ini, silahkan klik <a href="https://github.com/alfahrelrifananda/portfo/">disini</a>.</p>
         © <?= date('Y') ?> AlfahrelRifananda
     </footer>
 </body>
