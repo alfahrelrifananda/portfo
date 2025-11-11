97d6ed7781c22daff220efa87bafd7b0536d7006
alfahrelrifananda <pahrel1234@gmail.com>
2025-11-11 14:19:11 +0700

    update

--- about.php
+++ about.php
@@ -7,8 +7,11 @@ include 'header.php';
     
     <h2>Bio singkat</h2>
     <p>Nama saya Fahrel, dan saya adalah seorang web developer dan GNU/LINUX Advocate.</p>
+    <p>Saya berusia 20 tahun, dan berasal dari Surakarta, Jawa Tengah.</p>
+    <br>
+    <h2>Proyek</h2>
+    <p>Saya memiliki beberapa proyek berbasis web dan aplikasi mobile, berikut <a href="projects.php">diantaranya</a>.</p>
     
-    <p>Saya bertempat tinggal di Surakarta.</p>
     <h2>Skills</h2>
     <ul>
         <li>PHP & MySQL</li>
